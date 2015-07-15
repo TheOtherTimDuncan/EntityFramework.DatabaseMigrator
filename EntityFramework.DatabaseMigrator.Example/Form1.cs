e1ac292775e5ad272c3d82e8ce44ba6582d178e8
Tim <tduncan72@gmail.com>
2015-07-15 18:39:40 -0400

    Add solution with starting projects

---
+++ EntityFramework.DatabaseMigrator.Example/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EntityFramework.DatabaseMigrator.Example
+{
+    public partial class Form1 : DatabaseMigrator
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
