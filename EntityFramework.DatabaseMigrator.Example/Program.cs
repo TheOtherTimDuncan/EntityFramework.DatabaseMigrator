72e48dfc511ef1658ae6be37e52b6730b0e0dfb9
Tim <tduncan72@gmail.com>
2015-07-19 15:07:38 -0400

    Change example to use form from utility library

--- EntityFramework.DatabaseMigrator.Example/Program.cs
+++ EntityFramework.DatabaseMigrator.Example/Program.cs
@@ -15,7 +15,7 @@ namespace EntityFramework.DatabaseMigrator.Example
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DatabaseMigrator());
         }
     }
 }
