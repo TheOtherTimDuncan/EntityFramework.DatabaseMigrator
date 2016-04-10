a5a3c0c1a94ad40402e5afb59176ff9180a24549
Tim <tduncan72@gmail.com>
2016-04-10 15:48:04 -0400

    Update version to 1.1.0

--- PublishPackage.bat
+++ PublishPackage.bat
@@ -1,3 +1,3 @@
-"%~dp0\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe" push "%~dp0EntityFramework.DatabaseMigrator\bin\Debug\EntityFramework.DatabaseMigrator.1.0.0.0.nupkg"
-"%~dp0\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe" push "%~dp0EntityFramework.DatabaseMigrator\bin\Debug\EntityFramework.DatabaseMigrator.1.0.0.0.symbols.nupkg"
+"%~dp0\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe" push "%~dp0EntityFramework.DatabaseMigrator\bin\Debug\EntityFramework.DatabaseMigrator.1.1.0.0.nupkg"
+"%~dp0\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe" push "%~dp0EntityFramework.DatabaseMigrator\bin\Debug\EntityFramework.DatabaseMigrator.1.1.0.0.symbols.nupkg"
 pause
